5c92116bdb54d0e51d1dce92def882cf1cc5ddef
Yani Asari <yani.iman1@gmail.com>
2020-08-25 18:00:27 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
-  # code goes here
+  
+  attr_reader :name 
+  attr_accessor :owner, :mood 
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner 
+    @mood = "nervous"
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all  
+  end 
+  
 end
+
