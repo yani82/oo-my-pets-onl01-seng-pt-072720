da2bd76b911f55eebb0b2699282833319a1c3e78
Yani Asari <yani.iman1@gmail.com>
2020-08-25 18:00:08 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -58,4 +58,9 @@ class Owner
     end
   end 
   
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+    puts "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end 
+  
 end
