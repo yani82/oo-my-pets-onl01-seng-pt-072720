1eae004a68ba33661059f9f2c3488ce92b90a924
Yani Asari <yani.iman1@gmail.com>
2020-08-25 17:34:17 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,10 +3,17 @@ class Cat
   attr_reader :name 
   attr_accessor :owner 
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name 
     @owner = owner 
   end 
+  
+  def self.all 
+    @all << self 
+  end 
+  
   
   
 end
